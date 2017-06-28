9e75db573e4fc7b855bc25627a355b16cb2a963a
Bogdan Karapet <bogdankarapet@gmail.com>
2017-06-28 18:47:15 +0300

    Testing Yii features

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
@@ -122,5 +123,28 @@ class SiteController extends Controller
     public function actionAbout()
     {
         return $this->render('about');
+    }
+    
+    
+    /*example from yiiframework.com.ua*/
+     public function actionSay($message = 'Привіт')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
+    
+    public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // дані в $model успішно перевірені
+
+            // тут робимо щось корисне з $model ...
+ 
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // або сторінка відображається вперше, або ж є помилка в даних
+            return $this->render('entry', ['model' => $model]);
+        }
     }
 }
